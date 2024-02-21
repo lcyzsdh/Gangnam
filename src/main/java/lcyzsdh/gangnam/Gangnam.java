e982848e8c3ed48c4051b74475e8b6f82a92777e
luchenyang <1151468261@qq.com>
2024-02-21 14:51:23 +0800

    create.

---
+++ src/main/java/lcyzsdh/gangnam/Gangnam.java
@@ -0,0 +1,11 @@
+package lcyzsdh.gangnam;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("gangnam")
+public class Gangnam {
+    public static String MOD_ID="gangnam";
+    public Gangnam(){
+
+    }
+}
